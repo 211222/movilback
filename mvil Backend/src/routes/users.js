91a2409f1e974b069fef637269651f83bd4bcd94
203445 <203445@ids.upchiapas.edu.mx>
2024-04-07 15:30:57 -0600

    actualización back

--- mvil Backend/src/routes/users.js
+++ mvil Backend/src/routes/users.js
@@ -8,8 +8,20 @@ const pool = mysql.createPool({
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'danirachampo',
+    password: '12@3456a',
     database: 'movildentista'
+});
+
+// Ruta para obtener todos los pacientes
+router.get('/', (req, res) => {
+    pool.query('SELECT * FROM Paciente', (error, results) => {
+        if (error) {
+            console.error('Error al obtener los pacientes:', error);
+            res.status(500).json({ message: 'Hubo un error al obtener los pacientes.' });
+            return;
+        }
+        res.json(results);
+    });
 });
 
 // Ruta para obtener todos los pacientes
@@ -41,18 +53,50 @@ router.get('/:id', (req, res) => {
     });
 });
 
+// Ruta para obtener un usuario_paciente_detalles por ID
+router.get('/paciente/:id', (req, res) => {
+    const { id } = req.params;
+    pool.query('SELECT * FROM usuarios_registro WHERE id = ?', [id], (error, results) => {
+        if (error) {
+            console.error('Error al obtener el paciente:', error);
+            res.status(500).json({ message: 'Hubo un error al obtener el paciente.' });
+            return;
+        }
+        if (results.length === 0) {
+            res.status(404).json({ message: 'Paciente no encontrado.' });
+            return;
+        }
+        res.json(results[0]);
+    });
+});
+
 // Ruta para crear un nuevo paciente
-router.post('/', (req, res) => {
-    const { nombre, apellido, correo_electronico, contrasena, edad, tipo_sangre, enfermedades, alergias, medicamentos } = req.body;
-    pool.query('INSERT INTO Paciente (nombre, apellido, correo_electronico, contrasena, edad, tipo_sangre, enfermedades, alergias, medicamentos) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)', 
-    [nombre, apellido, correo_electronico, contrasena, edad, tipo_sangre, enfermedades, alergias, medicamentos], 
+router.post('/paciente', (req, res) => {
+    const { id_usuario_registro, edad, tipo_sangre, enfermedades, alergias, medicamentos } = req.body;
+    pool.query('INSERT INTO Paciente (id_usuario_registro, edad, tipo_sangre, enfermedades, alergias, medicamentos) VALUES (?, ?, ?, ?, ?, ?)', 
+    [id_usuario_registro, edad, tipo_sangre, enfermedades, alergias, medicamentos], 
     (error, results) => {
         if (error) {
             console.error('Error al crear el paciente:', error);
             res.status(500).json({ message: 'Hubo un error al crear el paciente.' });
             return;
         }
-        res.status(201).json({ message: 'Paciente creado exitosamente.', id: results.insertId });
+        res.status(200).json({ message: 'Paciente creado exitosamente.', id: results.insertId });
+    });
+});
+// Ruta para crear un nuevo usuario-paciente
+router.post('/', (req, res) => {
+    const { nombre, apellido, correo, contrasena, } = req.body;
+    pool.query('INSERT INTO usuarios_registro (nombre, apellido, correo, contrasena) VALUES (?, ?, ?, ?)', 
+    [nombre, apellido, correo, contrasena], 
+    (error, results) => {
+        if (error) {
+            console.error('Error al crear el paciente:', error);
+            res.status(500).json({ message: 'Hubo un error al crear el usuario.' });
+            return;
+        }
+        console.log(res);
+        res.status(200).json({ message: 'Paciente creado exitosamente.', id: results.insertId ,data: req.body });
     });
 });
 
